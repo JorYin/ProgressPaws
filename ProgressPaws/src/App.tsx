3fdd4386340441667e7048a2f469635a6fe816a6
JorYin <134739719+JorYin@users.noreply.github.com>
2024-06-03 20:43:50 -0500

    Basic Setup -> Supabase, Typescript + Vite,  and Tailwind CSS

---
+++ ProgressPaws/src/App.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+import { createClient } from "@supabase/supabase-js";
+import type {Database} from "./database.types";
+
+export const supabase = createClient<Database>(import.meta.env.VITE_API_SUPABASE_URL, import.meta.env.VITE_API_SUPABASE_KEY);
+
+type Country = Database['public']['Tables']['countries']['Row'];
+
+function App() {
+  const [countries, setCountries] = useState<Country[]>([]);
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  async function getCountries() {
+    const { data } = await supabase.from("countries").select();
+    setCountries(data || []);
+  }
+
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">
+          Countries in the database!
+        </h1>
+      <ul>
+        {countries.map((country) => (
+          <li key={country.name}>{country.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
